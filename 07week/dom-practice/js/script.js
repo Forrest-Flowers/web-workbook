ab4c978ce35ec90f179617b76658991cebbdc12b
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-08-27 18:43:38 -0500

    push2

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -12,20 +12,19 @@ window.onload = function(){
   document.querySelector('h1').insertAdjacentHTML('afterend', '<h2> You have ' + count.length + ' items in your cart. </h2>');
 
   var newInput = document.createElement('input');
+  newInput.innerHTML="Add Items"
   newInput.setAttribute('placeholder', "Type to add");
   let list = document.getElementsByTagName('ul');
   cart[0].after(newInput);
   newInput.setAttribute("id", "input-text");
 
-  let newButton = document.createElement('button');
-  newButton.innerHTML = 'Add Items';
-  newButton.setAttribute('id', 'add-button');
-  let nextButton - document.getElementsByTagName("input");
-  nextButton[0].after(newButton);
+  let newButton =
+  document.createElement('button');
+  newButton.innerHTML="Add items";
+  newButton.setAttribute("id", "add-button");
+  let nextButton =
+  document.getElementsByTagName("input");
 
-  newbutton.onClick = addItem;
-
-  addItem()
 
   // let list = document.getElementsByTagName('ul');
   //
