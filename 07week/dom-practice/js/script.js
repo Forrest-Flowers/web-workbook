99d38f3bc6199df75c7acfa89a17f8e3bde646fa
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-08-27 10:25:40 -0500

    First half done.

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+//
+// });
+
+window.onload = function(){
+
+  var count = document.getElementsByTagName('li')
+  alert('There are ' + count.length + ' items on your list.');
+
+  document.querySelector('h1').insertAdjacentHTML('afterend', '<h2> You have ' + count.length + ' items in your cart. </h2>');
+
+  var newInput = document.createElement('input');
+  newInput.setAttribute('placeholder', "Type to add");
+  let list = document.getElementsByTagName('ul');
+  cart[0].after(newInput);
+  newInput.setAttribute("id", "input-text");
+
+  let newButton = document.createElement('button');
+  newButton.innerHTML = 'Add Items';
+  newButton.setAttribute('id', 'add-button');
+  let nextButton - document.getElementsByTagName("input");
+  nextButton[0].after(newButton);
+
+  newbutton.onClick = addItem;
+
+  addItem()
+
+  // let list = document.getElementsByTagName('ul');
+  //
+  // let count = list[0].children.length;
+  // return(count);
+  //
+  // let cart = document.createElement('h2')
+  // cart.innerHTML="You have" count "in your cart."
+}
