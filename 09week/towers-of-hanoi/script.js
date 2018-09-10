326db8b37491104e938d975b0c416d331340168c
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-09-10 14:32:24 -0500

    Mostly done. Have weird Syntax Error

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
-});
+$(function() {
+      $('[data-block]').draggable({
+        revert: 'invalid'
+      });
+
+      $('[data-stack]').droppable({
+        drop: function(event, ui) {
+          let $last = ($(this).children().last()).data('block');
+          // console.log($last);
+
+          let $dragging = $(ui.draggable).data('block');
+          // console.log(`dragging element ${$dragging}`)
+
+          if ($dragging > $last) {
+            $(ui.draggable).draggable('option', 'revert', true);
+          } else {
+            $(ui.draggable).appendTo(this).attr('style', 'position: "relative"')
+          }
+          checkWin();
+        }
+      });
+
+      function checkWin(){
+        if ($("[data-stack=3]").children().length === 4){
+        $("#announce-game-won").text(`WINNER`);
+      }
+      }
+
+      $('#clear').click(function(){
+        $("#announce-game-won").empty();
+        location.reload();
+      })
